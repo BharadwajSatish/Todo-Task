1414ec02353e15af52c5c78a3683c7b37c83e614
VikrantSih <vkrntsingh20@gmail.com>
2021-04-01 06:18:15 +0530

    initial commit

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="footer">
+        @All Rights Reserved.
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+
+<style scoped>
+  #footer{
+     display:flex;
+        padding:10px;
+        justify-content: center;
+        background-color: bisque;
+        width:100%;
+        left: 0px;
+        position:fixed;
+        bottom:0px;
+        
+  }
+</style>
